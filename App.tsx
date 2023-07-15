51771db4d8193b01bbb842c4698043555e42afc0
berracode <juanguillermoalarcon@gmail.com>
2023-07-15 00:26:49 -0500

    se agrega drawer navigator funcional

--- App.tsx
+++ App.tsx
@@ -3,12 +3,14 @@ import 'react-native-gesture-handler';
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import StackNavigator from './src/navigation/StackNavigator';
+import { DrawerNavigator } from './src/navigation/DrawerNavigator';
 
 
 const App = () => {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      {/*<StackNavigator />*/}
+      <DrawerNavigator />
     </NavigationContainer>
   )
 }
